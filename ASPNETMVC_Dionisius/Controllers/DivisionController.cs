5c0e3f486e34213ab7935bd427ba7292ac983de0
dionisiusyose <dionisiusyose11@gmail.com>
2020-10-23 12:27:13 +0700

    CRUD basic

--- ASPNETMVC_Dionisius/Controllers/DivisionController.cs
+++ ASPNETMVC_Dionisius/Controllers/DivisionController.cs
@@ -2,6 +2,7 @@
 using ASPNETMVC_Dionisius.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -55,11 +56,39 @@ namespace ASPNETMVC_Dionisius.Controllers
             return View(division);
         }
 
-        //public ActionResult Edit(int id)
-        //{
-        //    Division division = myContext.Divisions.Find(id);
-        //    ViewBag.DepartmentId = new SelectList(myContext.Departments, "Id", "Name", division.DepartmentId);
-        //    return View(division);
-        //}
+        public ActionResult Edit(int id)
+        {
+            Division division = myContext.Divisions.Find(id);
+            ViewBag.DepartmentId = new SelectList(myContext.Departments, "Id", "Name", division.DepartmentId);
+            return View(division);
+        }
+
+        [HttpPost] [ValidateAntiForgeryToken]
+        public ActionResult Edit(Division division)
+        {
+            if (ModelState.IsValid)
+            {
+                myContext.Entry(division).State = EntityState.Modified;
+                myContext.SaveChanges();
+                return RedirectToAction("index");
+            }
+            ViewBag.DepartmentId = new SelectList(myContext.Departments, "Id", "Name", division.DepartmentId);
+            return View(division);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            var division = myContext.Divisions.Include("DepartmentIDNav").FirstOrDefault(m => m.Id == id);
+            return View(division);
+        }
+
+        [HttpPost, ActionName("Delete")][ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            var division = myContext.Divisions.Include("DepartmentIDNav").FirstOrDefault(d => d.Id == id);
+            myContext.Divisions.Remove(division);
+            myContext.SaveChanges();
+            return RedirectToAction("index");
+        }
     }
 }
